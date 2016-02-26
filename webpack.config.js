91ee02e4b2d9cbe70e6fa18af3b00c11d313db2b
Goutham Reddy <gouthamvreddy@gmail.com>
2016-02-26 13:33:28 -0800

    initial setup - added package.json, webpack config file, & babel presets

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{test: /\.jsx?$/, loader: "babel-loader", include: PATHS.app}
+    ]
+  }
+};
