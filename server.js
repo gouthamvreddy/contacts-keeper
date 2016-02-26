be410812815cbbd27fa4f1a50fe7e74563bf205a
Goutham Reddy <gouthamvreddy@gmail.com>
2016-02-26 14:07:30 -0800

    added express static server

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var port = process.env.PORT || 3000;
+var app = express();
+
+
+app.use(express.static(__dirname + '/build'));
+
+app.listen(port, function() {
+  console.log('Server running on port: ' + port);
+});
