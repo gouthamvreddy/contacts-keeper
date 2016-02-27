3499abca776e269c823f3f4695eb0c4372345acd
Goutham Reddy <gouthamvreddy@gmail.com>
2016-02-26 16:18:05 -0800

    added Header and App components to index.jsx, removed git tracking bundle.js

---
+++ app/components/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Header from './Header';
+
+class App extends React.Component {
+  render() {
+    return <Header />;
+  }
+}
---
+++ app/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export class Header extends React.Component {
+  render() {
+    return <header><h2>Contacts Keeper</h2></header>;
+  }
+}
